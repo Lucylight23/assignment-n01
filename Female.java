0cdfd9fc127fa29639b1cc62752e3865686c2498
Lucylight23 <167421729+Lucylight23@users.noreply.github.com>
2024-04-20 01:03:39 -0700

    Add files via upload

---
+++ Female.java
@@ -0,0 +1,18 @@
+2/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classfy;
+
+/**
+ *
+ * @author Luc_ylight
+ */
+public class Female {
+    void display(){
+        System.out.print("No ndevu: \n");
+         System.out.print("Shape: \n");
+          System.out.print("Have breast:");
+         
+    }}
+
---
+++ Male.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classfy;
+
+/**
+ *
+ * @author Luc_ylight
+ */
+public class Male {
+    void display(){
+        System.out.print("Have ndevu: \n");
+         System.out.print("Shapeless: \n");
+          System.out.print("No breast:");
+        
+    }
+    
+    
+    
+    
+}
