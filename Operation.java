136005279b8f96123088f5bf3fbf9bff60693ec8
Jakenya <167420976+clementina3-lucy@users.noreply.github.com>
2024-04-22 18:09:03 +0300

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author MWINJA
+ */
+public class Operation {
+      int a;
+    int b;
+    void add(){
+        int add=a+b;
+        System.out.print("sum is" +add);
+    }
+        void sub(){
+        int sub=a-b;
+        System.out.print("sub is" +sub);
+    }
+        void multy(){
+        int multy=a*b;
+        System.out.print("multy is" +multy);
+    }
+        void div(){
+        int div=a/b;
+        System.out.print("divs is" +div);
+    }
+    
+    
+}
+
+
+    
+
