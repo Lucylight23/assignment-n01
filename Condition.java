ae9f6d0df08d8e74e24842a60e1537a525f75fad
Jakenya <167420976+clementina3-lucy@users.noreply.github.com>
2024-04-22 18:08:18 +0300

    Add files via upload

---
+++ Condition.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package  ConditionalStatement1;
+
+/**
+ *
+ * @author MWINJA
+ */
+public class Condition {
+    int num= 0;
+    void check(){
+        if(num%2==0){
+        System.out.print("number is even");
+    }
+ else
+        {
+        System.out.print("number is odd");
+        }
+    }
+}
+    
+
