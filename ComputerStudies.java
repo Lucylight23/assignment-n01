5d5375b539c55d4e2971bbb53f0ce14c8d162534
Lucylight23 <167421729+Lucylight23@users.noreply.github.com>
2024-04-25 05:54:38 -0700

    Add files via upload

---
+++ ComputerStudies.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author PC
+ */
+class ComputerStudies extends DIT {
+   String module_name = "Object Oriented";
+   String module_code = "CSEU 07205";
+   
+   String module(){
+       System.out.println(": object oriented");
+       return module_name;
+   }
+
+    
+    
+}
